3e43be78ef53ebb6b010a922702480b08b5a775d
Muhammet <muhammetsarican@gmail.com>
2023-12-15 00:42:51 +0300

    I am still working on header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import './App.css'
 import { PageHeader } from './layouts/PageHeader'
 
 function App() {
   return (
-    <div className='flex flex-col'>
+    <div className='max-h-screen flex flex-col'>
       <PageHeader />
       <div>2</div>
     </div>
